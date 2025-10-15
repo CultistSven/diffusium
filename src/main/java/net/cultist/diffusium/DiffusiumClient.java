16626ad17dd673d0404af9939f19efc217f68c22
Cultist_Sven <61878352+CultistSven@users.noreply.github.com>
2025-10-15 18:04:41 +0200

    Initial commit

---
+++ src/main/java/net/cultist/diffusium/DiffusiumClient.java
@@ -0,0 +1,11 @@
+package net.cultist.diffusium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiffusiumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
